2d2a89f85bfed22c74604a48b99d9e5066d2f61b
wexort <bugwriter@outlook.com>
2019-12-05 13:03:56 +0100

    implement stack for pda

---
+++ PDA/pda.py
@@ -0,0 +1,6 @@
+from PDA.stack import Stack
+
+class PDAConfiguration:
+    def __init__(self, state, stack):
+        self.state = state
+        self.stack = stack
---
+++ PDA/stack.py
@@ -0,0 +1,26 @@
+class Stack:
+    """Purely functional non-destructive stack"""
+    def __init__(self, list_):
+        self.stk = [e for e in list_]
+    
+    def __str__(self):
+        rest = ''.join((char for char in self.stk[1:]))
+        return f'<Stack ({self.top()}){rest}>'
+    
+    def pop(self):
+        return Stack(self.stk[1:])
+
+    def push(self, character):
+        return Stack([character] + self.stk[:])
+
+    def top(self):
+        return self.stk[0]
+
+if __name__ == "__main__":
+    print('-' * 20)
+    stack = Stack(['a', 'b', 'c', 'd', 'e'])
+    print(stack)                                    #  <Stack (a)bcde>
+    print(stack.top())                              # a
+    print(stack.pop().pop().top())                  # c
+    print(stack.push('x').push('y').top())          # y
+    print(stack.push('x').push('y').pop().top())    # x
